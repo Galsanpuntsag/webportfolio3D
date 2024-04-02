72718975dbede2506f069d080fd1a38c2aec9290
Галсанпунцаг Бат-Орших <LP230092@G302-CC0108.local>
2024-04-02 17:45:29 +0800

    portfolio deploy

--- src/components/Home/Text/index.jsx
+++ src/components/Home/Text/index.jsx
@@ -1,8 +1,25 @@
 "use client";
 import React from "react";
 import Typewriter from "typewriter-effect";
+import { useRouter } from "next/navigation";
+
+const downloadCV = () => {
+  const url = "/images/myCV.pdf";
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", "Your_CV.pdf");
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
 
 export const Hometext = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/#about");
+  };
+
   return (
     <div className="flex flex-col shadow-2xl justify-center text-purple-300 items-center gap-2 2xl:gap-5 ">
       <h2 className="text-3xl lg:text-6xl 2xl:text-8xl">
@@ -30,11 +47,17 @@ export const Hometext = () => {
       </p>
 
       <div className="flex gap-4 mt-4">
-        <button className="btn btn-active bg-gradient-to-tr from-blue-900 to-purple-900 text-white">
+        <button
+          className="btn btn-active bg-gradient-to-tr from-blue-900 to-purple-900 text-white"
+          onClick={downloadCV}
+        >
           Download CV
         </button>
 
-        <button className="btn btn-active bg-gradient-to-tr  from-blue-900 to-purple-900 text-white w-28">
+        <button
+          className="btn btn-active bg-gradient-to-tr  from-blue-900 to-purple-900 text-white w-28"
+          onClick={handleClick}
+        >
           About Me
         </button>
       </div>
