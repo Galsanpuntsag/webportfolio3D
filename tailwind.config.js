0d449056cf60293ba58242124292bdf0eaa03da4
User <user@Users-MacBook-Air.local>
2024-04-03 11:26:12 +0800

    font family ynzalla

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,6 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      fontFamily: {
-        sans: ["Miss Fajardose", "cursive"],
-        abc: ["Flavors", "cursive"],
-        miss: ["Miss Fajardose", "cursive"],
-      },
     },
   },
   plugins: [require("daisyui")],
